a13ea15eda0910fce1acbefd11de93959ea04085
papillon27940 <112017555+papillon27940@users.noreply.github.com>
2026-05-31 14:53:40 +0200

    Update ticketButtons.js

--- src/handlers/ticketButtons.js
+++ src/handlers/ticketButtons.js
@@ -135,7 +135,7 @@ const createTicketHandler = {
 
       const reasonInput = new TextInputBuilder()
         .setCustomId('reason')
-        .setLabel('Why are you creating this ticket?')
+        .setLabel('')
         .setStyle(TextInputStyle.Paragraph)
         .setPlaceholder('Describe your issue...')
         .setRequired(true)
