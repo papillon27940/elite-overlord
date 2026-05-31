ddac081188b2ae9aa885654bfe6a7642f62286c7
papillon27940 <112017555+papillon27940@users.noreply.github.com>
2026-05-31 13:50:47 +0200

    Update ticketButtons.js

--- src/handlers/ticketButtons.js
+++ src/handlers/ticketButtons.js
@@ -135,7 +135,7 @@ const createTicketHandler = {
 
       const reasonInput = new TextInputBuilder()
         .setCustomId('reason')
-        .setLabel('merci de préciser ton nombre de bounty/region et que ton bluid et si c en 1V1 / 2V2 / 3V3')
+        .setLabel('nombre de bounty/region et ton bluid / si c en 1V1 / 2V2 / 3V3')
         .setStyle(TextInputStyle.Paragraph)
         .setPlaceholder('Describe your issue...')
         .setRequired(true)
